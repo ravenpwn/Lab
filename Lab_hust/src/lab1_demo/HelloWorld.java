37c7a332e22ed1189f14efa76c2b115c8aa22a53
Trung Nguyen <128338386+ravenpwn@users.noreply.github.com>
2023-04-25 18:59:03 +0700

    Delete old files

--- Lab_hust/src/lab1_demo/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package lab1_demo;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
--- Lab_hust/src/lab1_demo/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author trung
- *
- */
-package lab1_demo;
